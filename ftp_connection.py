8994d05966f27c4a3213862dc54fc9b53ef79b05
Mayank Tiwari <70434094+mktece@users.noreply.github.com>
2021-07-22 16:53:49 +0530

    Update ftp_connection.py

--- ftp_connection.py
+++ ftp_connection.py
@@ -3,7 +3,7 @@ from ftplib import FTP
 
 ftp = FTP('ftp.cwi.nl')   # connect to host, default port
 
-ftp.login()               # user anonymous, passwd anonymous@
+ftp.login()               # user <username>, passwd <password>
 
 ftp.retrlines('LIST')     # list directory contents 
 
